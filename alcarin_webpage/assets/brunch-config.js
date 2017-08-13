060026cd0b48a8dd8df5f4b5fa03ea71c572a30b
psychowico <psychowico@alcarin.org>
2017-08-13 07:59:20 +0200

    installing vue.js as base js framework and pure.css as css framework

--- alcarin_webpage/assets/brunch-config.js
+++ alcarin_webpage/assets/brunch-config.js
@@ -2,14 +2,14 @@ exports.config = {
   // See http://brunch.io/#documentation for docs.
   files: {
     javascripts: {
-      joinTo: "js/app.js"
+      // joinTo: "js/app.js"
 
       // To use a separate vendor.js bundle, specify two files path
       // http://brunch.io/docs/config#-files-
-      // joinTo: {
-      //   "js/app.js": /^js/,
-      //   "js/vendor.js": /^(?!js)/
-      // }
+      joinTo: {
+        "js/app.js": /^js/,
+        "js/vendor.js": /^(?!js)/
+      }
       //
       // To change the order of concatenation of files, explicitly mention here
       // order: {
@@ -57,6 +57,9 @@ exports.config = {
   },
 
   npm: {
-    enabled: true
+    enabled: true,
+    aliases: {
+      vue: 'vue/dist/vue.common.js'
+    }
   }
 };
